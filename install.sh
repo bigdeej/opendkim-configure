08ed0d39ceeb1c75bb1e710e127ac54f509b4e5f
Big Deej <github@gcosoftware.com>
2016-07-25 19:34:03 -0400

    Fix permissions

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ cp -a ./req/opendkim-configure /usr/sbin/opendkim-configure
 cp -a ./req/opendkim-configure.conf.php /usr/sbin/opendkim-configure.conf.php
 cp -a ./req/opendkim-configure.php /usr/sbin/opendkim-configure.php
 
+echo "Setting permissions..."
+
+chmod +x /usr/sbin/opendkim-configure
+
 echo "Done!"
 echo "You can now run using the command opendkim-configure"
 echo ""
